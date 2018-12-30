f8bb92e58ce95ae5d57f60e1f80d0ca306bffd2b
Finnerale <git@leopoldluley.de>
2018-12-30 14:31:09 +0100

    Make UiRender position components relative to their parent

--- src/ui/render.rs
+++ src/ui/render.rs
@@ -1,5 +1,4 @@
-use crate::{Bounds, Cid, Renderer, UiData};
-use log::trace;
+use crate::{Bounds, Cid, Renderer, UiData, Position};
 
 pub struct UiRender<'a> {
     data: &'a UiData,
@@ -8,26 +7,27 @@ pub struct UiRender<'a> {
 
 impl<'a> UiRender<'a> {
     pub(crate) fn run(data: &'a UiData, renderer: &mut Renderer, root: Cid) {
-        trace!("Running `UiRender`");
+        log::trace!("Running `UiRender`");
         let mut ui = UiRender { data, renderer };
-        ui.render(root);
+        ui.render(root, Position::default());
     }
 
-    pub fn render(&mut self, cid: Cid) {
-        {
-            let pointer = self.data.pointer[cid.get()];
-            let state = self.data.state[cid.get()].as_ref().unwrap();
-            let position = self.data.position[cid.get()];
-            let size = self.data.size[cid.get()];
-            let bounds = Bounds::new(position, size);
+    pub fn render(&mut self, cid: Cid, offset: Position) {
+        let pointer = self.data.pointer[cid.get()];
+        let position = self.data.position[cid.get()] + offset;
+        let size = self.data.size[cid.get()];
+        let bounds = Bounds::new(position, size);
+        log::trace!("Rendering {:?} at {:?} with {:?}", cid, position, size);
 
+        {
+            let state = self.data.state[cid.get()].as_ref().unwrap();
             (pointer.render)(state, bounds, self.renderer);
         }
 
         let children = &self.data.children[cid.get()];
 
         for child in children {
-            self.render(*child);
+            self.render(*child, position);
         }
     }
 }
