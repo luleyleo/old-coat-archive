21f47acee8a130f2a73c989a456de8b8e9367eb5
Finnerale <acc@leopoldluley.de>
2019-05-24 21:33:22 +0200

    Disable TextEdit widget for now

--- src/widgets.rs
+++ src/widgets.rs
@@ -4,8 +4,8 @@ pub use self::rectangle::Rectangle;
 mod text;
 pub use self::text::Text;
 
-mod textedit;
-pub use self::textedit::TextEdit;
+// mod textedit;
+// pub use self::textedit::TextEdit;
 
 mod touch_area;
 pub use self::touch_area::{TouchArea, TouchAreaEvent};
